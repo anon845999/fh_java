68dcfd6bec37498446223469a8e9867b90cb0e4c
anon <q845999@gmail.com>
2025-04-04 18:47:49 +0200

    bissl besser geschrieben, aber sollte passen wenn ich mich nicht irre..coder machen ja nie Fehler hahah

--- ArrayEvenStatistics/src/Main.java
+++ ArrayEvenStatistics/src/Main.java
@@ -29,62 +29,70 @@ public class Main {
         ArrayList<BigInteger> evenNumbers = new ArrayList<BigInteger>();
         ArrayList<Integer> indexOfEvenNumbers = new ArrayList<Integer>();
 
+
+
         //getting the indexes of even numbers in my array and storing evennumbers and index in array
         for (int i = 0; i < arr.length; i++) {
             if(arr[i].mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
-                evenNumbers.add(BigInteger.valueOf(i));
+                evenNumbers.add(arr[i]);
                 indexOfEvenNumbers.add(i);
                  // number of even numbers stored (counter)
             }
         }
-
         //writing out the indexcounter, how many even numbers do we have
         System.out.println("even numbers counter: " + indexOfEvenNumbers.size());
 
         //printing out the indexes of the even numbers from my array
+        System.out.println("even numbers indices: ");
         for(var item : indexOfEvenNumbers){
-            int index = indexOfEvenNumbers.indexOf(item);
-            System.out.println("even indexes: " + item);
+            System.out.println(item);
         }
+        System.out.println();
 
         //sum of the even numbers --> into BigInteger sumOfEvenNumbers = BigInteger.ZERO;
-        BigInteger sumOfEvenNumbers = new BigInteger("0");
+        BigInteger sumOfEvenNumbers = BigInteger.ZERO;
         for (var item : evenNumbers) {
             sumOfEvenNumbers = sumOfEvenNumbers.add(item);
         }
-
+        System.out.println("sum of even numbers: " + sumOfEvenNumbers);
 
         //mean of the even numbers
         BigInteger meanOfEvenNumbers = BigInteger.ZERO;
 
-
+        if(!evenNumbers.isEmpty()){
             meanOfEvenNumbers = sumOfEvenNumbers.divide(BigInteger.valueOf(evenNumbers.size()));
+        }else{
+            System.out.println("even numbers list is empty");
+        }
+
 
         System.out.println("mean of even numbers: " + meanOfEvenNumbers);
 
         //value of the maximum even number
-
-
-     //  BigInteger maxOfEvenNumbers = evenNumbers.isEmpty() ? null : evenNumbers.stream().max(BigInteger::compareTo).get();
+        //  BigInteger maxOfEvenNumbers = evenNumbers.isEmpty() ? null : evenNumbers.stream().max(BigInteger::compareTo).get();
         BigInteger maxOfEvenNumbers = Collections.max(evenNumbers);
 
-       System.out.println("max of even numbers: " + maxOfEvenNumbers);
+        if(maxOfEvenNumbers != null){
+            System.out.println("max of even numbers: " + maxOfEvenNumbers);
+        }
 
        //  (all) index position(s) of the maximum even number
-        ArrayList<BigInteger> maxOfEvenNumbersList = new ArrayList<>();
+        ArrayList<BigInteger> indecesMaxOfEvenNumbersList = new ArrayList<>();
 
         for (var item : evenNumbers) {
             if(item.equals(maxOfEvenNumbers)){
-                maxOfEvenNumbersList.add(item);
-                System.out.println("max of even numbers: " + maxOfEvenNumbersList);
+                indecesMaxOfEvenNumbersList.add(item);
             }
         }
 
-        for(var item : indexOfEvenNumbers){
-            System.out.println("indexes of the even max numbers " + item);
+        for(var item : indecesMaxOfEvenNumbersList){
+            System.out.println("indices of the even max numbers " + item);
         }
 
-
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
 
     }
